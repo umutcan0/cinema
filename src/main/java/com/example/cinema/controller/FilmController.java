8e518f9d0fc3be543965ca9c5ab6be3fa82b9209
umut.bal <umut.bal@etiya.com>
2023-12-24 16:53:26 +0300

    FilmController url updated.

--- src/main/java/com/example/cinema/controller/FilmController.java
+++ src/main/java/com/example/cinema/controller/FilmController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Film")
+@RequestMapping("/film")
 public class FilmController {
 
     @Autowired
